89983ec99190f2367d31a47af38a3df7928c5295
ymaulana <y.maulana1609@gmail.com>
2022-11-28 17:42:30 +0700

    features: add function: counter, add item, remove item, checkout total item

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -6,12 +8,25 @@ import { ListItem } from "./components/ListItem";
 import items from "./dummy/items.json";
 
 function App() {
+  const [listItems, setListItems] = useState([]);
+  const [totalItem, setTotalItem] = useState(0);
+
+  const sumTotal = (allItems) => {
+    const total = allItems.reduce((prev, curr) => prev + curr.value, 0);
+    setTotalItem(total);
+  };
+
+  useEffect(() => {
+    setListItems(items);
+    sumTotal(items);
+  }, []);
+
   return (
     <div className="App">
       <Container>
-        <Header />
+        <Header totalItem={totalItem} />
         <Body>
-          <ListItem items={items} />
+          <ListItem items={listItems} />
         </Body>
         <Footer />
       </Container>
